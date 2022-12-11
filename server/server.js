15a4830a3e6e7abe955d1cecd724796475e7b817
briabr <filalibria@yahoo.fr>
2022-12-10 20:10:29 -0500

    debugged auth.js

--- server/server.js
+++ server/server.js
@@ -16,9 +16,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", (req,res) => {
-res.sendFile(path.join(__dirname,"../client/build/index.html"))
-})
+// app.get("*", (req,res) => {
+// res.sendFile(path.join(__dirname,"../client/build/index.html"))
+// })
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
