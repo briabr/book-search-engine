3e63a6c3dcd769e8a4388304e58e7d537e4e91b2
briabr <filalibria@yahoo.fr>
2022-12-10 19:32:20 -0500

    added app.get() route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("*", (req,res) => {
+res.sendFile(path.join(__dirname,"../client/build/index.html"))
+})
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
